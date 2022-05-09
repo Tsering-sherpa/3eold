e647385615f798dfe2fa22f4435c5602e25dc0b4
binchin <silwalsachindra25@gmail.com>
2022-05-09 20:47:09 +0545

    navbar and footer component generated

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    NavBarComponent
+  ],
   imports: [
     CommonModule
   ]
