2522c40aa6c6f719741d4c80b43dc80c746cd74f
Tsering Wongdi Sherpa <tseringwongdisherpa@bajratechnologies.com>
2022-05-13 16:01:14 +0545

    Feat: Add new component for blog and destination card

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { BlogDestinationCardComponent } from './components/blog-destination-card/blog-destination-card.component';
 
 @NgModule({
-  declarations: [FooterComponent, NavBarComponent],
+  declarations: [FooterComponent, NavBarComponent, BlogDestinationCardComponent],
   imports: [CommonModule],
   exports: [FooterComponent, NavBarComponent],
 })
