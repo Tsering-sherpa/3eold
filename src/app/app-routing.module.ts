d0f01bf3f86ae766c40057a55597294efa11bc97
binchin <silwalsachindra25@gmail.com>
2022-05-21 19:58:45 +0545

    about us page layout created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'about', component: AboutUsComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
